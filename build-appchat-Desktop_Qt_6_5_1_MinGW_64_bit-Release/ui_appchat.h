bacd3e4964e4f6867068e4169e199fe899dcbb1d
ngocson2g <ngocson1265@gmail.com>
2023-06-14 16:26:31 +0700

    cv 14-6

--- build-appchat-Desktop_Qt_6_5_1_MinGW_64_bit-Release/ui_appchat.h
+++ build-appchat-Desktop_Qt_6_5_1_MinGW_64_bit-Release/ui_appchat.h
@@ -18,6 +18,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QStackedWidget>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
@@ -28,7 +29,12 @@ class Ui_AppChat
 {
 public:
     QWidget *centralwidget;
-    QWidget *widget;
+    QGridLayout *gridLayout_2;
+    QStackedWidget *stackedWidget;
+    QWidget *page;
+    QPushButton *pushButton_open_app;
+    QWidget *page_2;
+    QGridLayout *gridLayout_3;
     QGridLayout *gridLayout;
     QLabel *label;
     QLabel *label_index_code;
@@ -37,6 +43,7 @@ public:
     QTextEdit *textEdit_recied;
     QPushButton *pushButton_Send;
     QLineEdit *lineEdit_Message_Send;
+    QPushButton *pushButton_back;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -44,54 +51,76 @@ public:
     {
         if (AppChat->objectName().isEmpty())
             AppChat->setObjectName("AppChat");
-        AppChat->resize(285, 370);
+        AppChat->resize(926, 623);
         centralwidget = new QWidget(AppChat);
         centralwidget->setObjectName("centralwidget");
-        widget = new QWidget(centralwidget);
-        widget->setObjectName("widget");
-        widget->setGeometry(QRect(11, 44, 258, 276));
-        gridLayout = new QGridLayout(widget);
+        gridLayout_2 = new QGridLayout(centralwidget);
+        gridLayout_2->setObjectName("gridLayout_2");
+        stackedWidget = new QStackedWidget(centralwidget);
+        stackedWidget->setObjectName("stackedWidget");
+        page = new QWidget();
+        page->setObjectName("page");
+        pushButton_open_app = new QPushButton(page);
+        pushButton_open_app->setObjectName("pushButton_open_app");
+        pushButton_open_app->setGeometry(QRect(280, 150, 80, 61));
+        stackedWidget->addWidget(page);
+        page_2 = new QWidget();
+        page_2->setObjectName("page_2");
+        gridLayout_3 = new QGridLayout(page_2);
+        gridLayout_3->setObjectName("gridLayout_3");
+        gridLayout = new QGridLayout();
         gridLayout->setObjectName("gridLayout");
-        gridLayout->setContentsMargins(0, 0, 0, 0);
-        label = new QLabel(widget);
+        label = new QLabel(page_2);
         label->setObjectName("label");
 
         gridLayout->addWidget(label, 0, 0, 1, 1);
 
-        label_index_code = new QLabel(widget);
+        label_index_code = new QLabel(page_2);
         label_index_code->setObjectName("label_index_code");
 
         gridLayout->addWidget(label_index_code, 0, 2, 1, 1);
 
-        comboBox_ListAddress = new QComboBox(widget);
+        comboBox_ListAddress = new QComboBox(page_2);
         comboBox_ListAddress->setObjectName("comboBox_ListAddress");
 
         gridLayout->addWidget(comboBox_ListAddress, 1, 1, 1, 1);
 
-        pushButton_Connect = new QPushButton(widget);
+        pushButton_Connect = new QPushButton(page_2);
         pushButton_Connect->setObjectName("pushButton_Connect");
 
         gridLayout->addWidget(pushButton_Connect, 1, 2, 1, 1);
 
-        textEdit_recied = new QTextEdit(widget);
+        textEdit_recied = new QTextEdit(page_2);
         textEdit_recied->setObjectName("textEdit_recied");
 
         gridLayout->addWidget(textEdit_recied, 2, 0, 1, 3);
 
-        pushButton_Send = new QPushButton(widget);
+        pushButton_Send = new QPushButton(page_2);
         pushButton_Send->setObjectName("pushButton_Send");
 
         gridLayout->addWidget(pushButton_Send, 3, 2, 1, 1);
 
-        lineEdit_Message_Send = new QLineEdit(widget);
+        lineEdit_Message_Send = new QLineEdit(page_2);
         lineEdit_Message_Send->setObjectName("lineEdit_Message_Send");
 
         gridLayout->addWidget(lineEdit_Message_Send, 3, 0, 1, 2);
 
+        pushButton_back = new QPushButton(page_2);
+        pushButton_back->setObjectName("pushButton_back");
+
+        gridLayout->addWidget(pushButton_back, 1, 0, 1, 1);
+
+
+        gridLayout_3->addLayout(gridLayout, 0, 0, 1, 1);
+
+        stackedWidget->addWidget(page_2);
+
+        gridLayout_2->addWidget(stackedWidget, 0, 0, 1, 1);
+
         AppChat->setCentralWidget(centralwidget);
         menubar = new QMenuBar(AppChat);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 285, 21));
+        menubar->setGeometry(QRect(0, 0, 926, 21));
         AppChat->setMenuBar(menubar);
         statusbar = new QStatusBar(AppChat);
         statusbar->setObjectName("statusbar");
@@ -99,16 +128,21 @@ public:
 
         retranslateUi(AppChat);
 
+        stackedWidget->setCurrentIndex(0);
+
+
         QMetaObject::connectSlotsByName(AppChat);
     } // setupUi
 
     void retranslateUi(QMainWindow *AppChat)
     {
         AppChat->setWindowTitle(QCoreApplication::translate("AppChat", "AppChat", nullptr));
+        pushButton_open_app->setText(QCoreApplication::translate("AppChat", "App chat", nullptr));
         label->setText(QCoreApplication::translate("AppChat", "APP CHAT", nullptr));
         label_index_code->setText(QCoreApplication::translate("AppChat", "Code", nullptr));
         pushButton_Connect->setText(QCoreApplication::translate("AppChat", "connect", nullptr));
         pushButton_Send->setText(QCoreApplication::translate("AppChat", "Send", nullptr));
+        pushButton_back->setText(QCoreApplication::translate("AppChat", "back", nullptr));
     } // retranslateUi
 
 };
